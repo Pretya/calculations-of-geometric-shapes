70624df92ff0605732689892dd0db6c4cb5934a9
Yurii Shulika <jurgen12344@gmail.com>
2023-03-07 16:37:31 +0200

    changing variable names

--- domOperations.js
+++ domOperations.js
@@ -1,28 +1,28 @@
 
 
 function showResultInDomKvadrat() {
-  const squer = document.getElementById('dlinaStoroniKvadrata').value;
+  const squerSide = document.getElementById('dlinaStoroniKvadrata').value;
   const kvadrat = document.getElementById('ploschadKvadrata');
-  kvadrat.innerHTML = squareArea(squer)
+  kvadrat.innerHTML = squareArea(squerSide)
 }
 
 function showResultInDomPriamougolnika() {
-  const rectangleA = document.getElementById('pervayaStoronaPriamougolnika').value;
-  const rectangleB = document.getElementById('drugayaStoronaPriamougolnika').value;
+  const rectangleSideOne = document.getElementById('pervayaStoronaPriamougolnika').value;
+  const rectangleSideTwo = document.getElementById('drugayaStoronaPriamougolnika').value;
   const priamougolnik = document.getElementById('ploschadPriamougolnika');
-  priamougolnik.innerHTML = rectangleArea(rectangleA, rectangleB)
+  priamougolnik.innerHTML = rectangleArea(rectangleSideOne, rectangleSideTwo)
 }
 
 function showResultInDomParalelograma() {
-  const parallelogramA = document.getElementById('dlinaStoronyParalelograma').value;
-  const parallelogramH = document.getElementById('dlinaVisotyParalelograma').value;
+  const parallelogramSide = document.getElementById('dlinaStoronyParalelograma').value;
+  const parallelogramHeight = document.getElementById('dlinaVisotyParalelograma').value;
   const paralelogram = document.getElementById('ploschadParalelograma');
-  paralelogram.innerHTML = parallelogramArea(parallelogramA, parallelogramH)
+  paralelogram.innerHTML = parallelogramArea(parallelogramSide, parallelogramHeight)
 }
 
 function showResultInDomRomba() {
-  const rhombusA = document.getElementById('dlinaStoronyRomba').value;
-  const rhombusH = document.getElementById('dlinaVisotyRomba').value;
+  const rhombusSide = document.getElementById('dlinaStoronyRomba').value;
+  const rhombusHeight = document.getElementById('dlinaVisotyRomba').value;
   const romb = document.getElementById('ploschadRomba');
-  romb.innerHTML = rhombusArea(rhombusA, rhombusH)
+  romb.innerHTML = rhombusArea(rhombusSide, rhombusHeight)
 }
